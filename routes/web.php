1108ed851f730d73d3d316f71b2a12542a2ac177
suptohossein <supto.hossein@gmail.com>
2022-10-22 01:39:46 +0600

    initial project, create new branch & remove blank space on web.php

--- routes/web.php
+++ routes/web.php
@@ -8,23 +8,7 @@ use App\Http\Controllers\Backend\MemberController;
 use App\Http\Controllers\HomeController;
 
 
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
-
-// test route
-// Route::get('/demo', function(){
-//     $data = ['name'=> 'sitename', 'url'=>'dhanmondisociety'];
-//     return view('admin.test.test', compact('data'));
-// });
-
-
-
 Route::view('/test', 'test');
-
 
 
 // Frontend Routes
@@ -35,10 +19,6 @@ Route::get('about', [FrontendController::class, 'about'])->name('website.about')
 Route::get('/contact', [FrontendController::class, 'contact'])->name('website.contact');
 Route::get('/complain', [FrontendController::class, 'complain'])->name('website.complain');
 // Route::get('/executiveMember', [FrontendController::class, 'executiveMember'])->name('website.executiveMember');
-
-
-
-
 
 
 // Backend Routes
@@ -52,24 +32,9 @@ Route::resource('/member', MemberController::class);
 
 
 
-
-// test for git command
-// Route::view('/error', 'view.error');
-
-
 // Auth Routes
 Auth::routes(['register' => false]);
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 
-
-
-
-
-
-// </div>
-//     <div class="col-4">
-//         <button id="trashButton" class="btn btn-danger btn-small"><i
-//                 class="fa-solid fa-trash"></i></button>
-//     </div>
