312fd368cc8543f2f05ceefa3ba052c55b5adc0c
Lutfunnaher <l.lata710@gmail.com>
2022-10-21 16:50:45 +0600

    odify member type sidebar

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,3 +1,6 @@
+@php use App\Models\MembershipCategory; @endphp
+
+
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
       <a href="index.html" class="app-brand-link">
@@ -91,40 +94,22 @@
         </a>
       </li>
 
-
-      <!-- Layouts -->
+      {{-- member --}}
       <li class="menu-item">
         <a href="javascript:void(0);" class="menu-link menu-toggle">
           <i class="menu-icon tf-icons bx bx-layout"></i>
-          <div data-i18n="Layouts">Layouts</div>
+          <div data-i18n="Layouts">Member Type</div>
         </a>
 
+
         <ul class="menu-sub">
-          <li class="menu-item">
-            <a href="layouts-without-menu.html" class="menu-link">
-              <div data-i18n="Without menu">Without menu</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="layouts-without-navbar.html" class="menu-link">
-              <div data-i18n="Without navbar">Without navbar</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="layouts-container.html" class="menu-link">
-              <div data-i18n="Container">Container</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="layouts-fluid.html" class="menu-link">
-              <div data-i18n="Fluid">Fluid</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="layouts-blank.html" class="menu-link">
-              <div data-i18n="Blank">Blank</div>
-            </a>
-          </li>
+            @foreach (MembershipCategory::all() as $member_type)
+            <li class="menu-item">
+                <a href="#" class="menu-link">
+                <div data-i18n="Without menu">{{ $member_type->name }} Member</div>
+                </a>
+            </li>
+            @endforeach
         </ul>
       </li>
 
