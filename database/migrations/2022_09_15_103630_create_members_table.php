bd268bd3af2578ab59ec6741251031285fab043c
wafi <l.lata710@gmail.com>
2022-11-16 22:52:03 +0600

    Table structure changed

--- database/migrations/2022_09_15_103630_create_members_table.php
+++ database/migrations/2022_09_15_103630_create_members_table.php
@@ -17,29 +17,29 @@ class CreateMembersTable extends Migration
             // member
             $table->id();
             $table->string('membership_number');
-            $table->string('current_membership_number');
+            $table->string('current_membership_number')->nullable();
             // introduce
             $table->string('introduce_member_id')->nullable();
-            $table->string('member_type')->default('non-executive');
+            $table->string('member_type');
             $table->string('membership_category_id');
             $table->string('name');
             $table->string('position')->nullable();
-            $table->string('father_name');
+            $table->string('father_name')->nullable();
             $table->string('mother_name')->nullable();
             $table->string('spouse_ame')->nullable();
             $table->string('road_no')->nullable();
             $table->text('address')->nullable();
-            $table->string('nid_no');
+            $table->string('nid_no')->nullable();
             $table->string('nationality')->nullable();
             $table->string('occupation')->nullable();
             $table->string('blood_group')->nullable();
             $table->string('marital_status')->nullable();
-            $table->string('date_of_birth');
+            $table->string('date_of_birth')->nullable();
             $table->string('date_of_marriage')->nullable();
             $table->string('telephone_res')->nullable();
             $table->string('workplace')->nullable();
-            $table->string('mobile')->unique();
-            $table->string('email')->unique();
+            $table->string('mobile')->nullable();
+            $table->string('email')->nullable();
             // contact person
             $table->string('contact_person_name')->nullable();
             $table->string('relation')->nullable();
