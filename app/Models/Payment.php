a5cfaf4d5a3c72262fe6fb1814c490f15cd87f25
wafi <l.lata710@gmail.com>
2022-11-20 21:50:43 +0600

    create relationshio on member

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['created_at', 'updated_at'];
+
+
+    public function member()
+    {
+        $this->belongsTo(Member::class);
+    }
 }
