b00ea4e3af073e259bcfe2aee217f1600f348e56
wafi <l.lata710@gmail.com>
2022-11-20 21:49:42 +0600

    modify store method

--- app/Http/Controllers/Backend/MemberController.php
+++ app/Http/Controllers/Backend/MemberController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Member;
 use App\Models\MembershipCategory;
 use App\Models\Children;
+use App\Models\Payment;
 use Illuminate\Http\Request;
 
 class MemberController extends Controller
@@ -17,7 +18,8 @@ class MemberController extends Controller
      */
     public function index()
     {
-
+        $members = Member::with('children')->get();
+        ddd($members);
     }
 
     /**
@@ -43,103 +45,77 @@ class MemberController extends Controller
         // dd($request->input());
         // return $request->membership_type;
         // $membership_id = 'GM'.$request->membership_id;
+
+        // membership_type check
         if($request->membership_type == 'executive') {
             $member_type = $request->membership_type;
         } else {
             $member_type = 'non-executive';
         }
 
-            Member::create([
-                'name' => $request->name,
-                'father_name' => $request->father_name,
-                'mother_name' => $request->mother_name,
-                'spouse_ame' => $request->spouse_ame,
-                'membership_number' => 'GM' . $request->membership_number,
-                'current_membership_number' => 'GM' . $request->membership_number,
-                'road_no' => $request->road_no,
-                'address' => $request->address,
-                'nid_no' => $request->nid_no,
-                'nationality' => $request->nationality,
-                'occupation' => $request->occupation,
-                'blood_group' => $request->blood_group,
-                'member_type' => $member_type,
-                'marital_status' => $request->marital_status,
-                'date_of_birth' => $request->date_of_birth,
-                'date_of_marriage' => $request->date_of_marriage,
-                'telephone_res' => $request->telephone_res,
-                'mobile' => $request->mobile,
-                'workplace' => $request->workplace,
-                'email' => $request->email,
-                'contact_person_name' => $request->contact_person_name,
-                'relation' => $request->relation,
-                'contact_person_telephone' => $request->contact_person_telephone,
-                'contact_person_mobile' => $request->contact_person_mobile,
-                'membership_category_id' => $request->category_of_membership,
-                'introduce_member_id' => $request->introduce_member_id,
-                'how_long_known' => $request->how_long_known,
-            ]);
+        // insert data to member table
+        Member::create([
+            'name' => $request->name,
+            'father_name' => $request->father_name,
+            'mother_name' => $request->mother_name,
+            'spouse_ame' => $request->spouse_ame,
+            'membership_number' => 'GM' . $request->membership_number,
+            'current_membership_number' => 'GM' . $request->membership_number,
+            'road_no' => $request->road_no,
+            'address' => $request->address,
+            'nid_no' => $request->nid_no,
+            'nationality' => $request->nationality,
+            'occupation' => $request->occupation,
+            'blood_group' => $request->blood_group,
+            'member_type' => $member_type,
+            'marital_status' => $request->marital_status,
+            'date_of_birth' => $request->date_of_birth,
+            'date_of_marriage' => $request->date_of_marriage,
+            'telephone_res' => $request->telephone_res,
+            'mobile' => $request->mobile,
+            'workplace' => $request->workplace,
+            'email' => $request->email,
+            'contact_person_name' => $request->contact_person_name,
+            'relation' => $request->relation,
+            'contact_person_telephone' => $request->contact_person_telephone,
+            'contact_person_mobile' => $request->contact_person_mobile,
+            'membership_category_id' => $request->category_of_membership,
+            'introduce_member_id' => $request->introduce_member_id,
+            'how_long_known' => $request->how_long_known,
+        ]);
+
+        // insert data to children table
+        if(!empty($request->children_name)) {
+            $children_name = $request->children_name;
+            $gender = $request->gender;
+            $age = $request->age;
+
+            $array = [$children_name, $gender, $age];
+            $count = count($children_name);
+
+            for($i=0; $i<=$count-1; $i++) {
+                $child_info = array_column($array, $i);
+
+                Children::create([
+                    'membership_number' => 'GM' . $request->membership_number,
+                    'children_name' => $child_info[0],
+                    'gender' => $child_info[1],
+                    'age' => $child_info[2],
+                ]);
+            }
+        }
 
 
-
-
-        // $children_name = $request->children_name;
-        // $gender = $request->gender;
-        // $age = $request->age;
-
-        // $array = [$children_name, $gender, $age];
-        // $count = count($children_name);
-        // dd($count); 4
-        // $array 3
-
-        // for($i=0; $i<=$count-1; $i++) {
-        //     $child_info = array_column($array, $i);
-        //     print_r($child_info);
-
-        //     Children::create([
-        //         'member_id' => $request->membership_id,
-        //         'name' => $child_info[0],
-        //         'gender' => $child_info[1],
-        //         'age' => $child_info[2],
-        //     ]);
-        // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /**
-         * Member Children insertion
-         *
-         */
-        // $name = $request->name;
-        // $gender = $request->gender;
-        // $age = $request->age;
-
-        // $array = [$name, $gender, $age];
-        // $count = count($name);
-        // dd($count); 4
-        // $array 3
-
-        // for($i=0; $i<=$count-1; $i++) {
-        //     $child_info = array_column($array, $i);
-        //     // print_r($child_info);
-
-        //     Children::create([
-        //         'member_id' => 1,
-        //         'name' => $child_info[0],
-        //         'gender' => $child_info[1],
-        //         'age' => $child_info[2],
-        //     ]);
-        // }
-
+        // insert data to payment table
+        Payment::create([
+            'membership_number' => 'GM' . $request->membership_number,
+            'category_of_membership' => $request->category_of_membership,
+            'payment_type' => $request->payment_type,
+            'payment_date' => $request->payment_date,
+            'bank_name' => $request->bank_name,
+            'cheque_number' => $request->cheque_number,
+            'cheque_date' => $request->cheque_date,
+        ]);
 
         return redirect()->back();
     }
