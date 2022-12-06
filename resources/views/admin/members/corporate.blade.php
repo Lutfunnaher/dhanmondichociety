f4ecd685ade89cd28506035eabd81238d14e4e9d
SuptoHossein <supto.hossei@gmail.com>
2022-12-06 22:51:56 +0600

    create member categories blade files

---
+++ resources/views/admin/members/corporate.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.backend')
+
+@section('styles')
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
+@endsection
+
+@section('content')
+<div class="container-xxl flex-grow-1 container-p-y">
+    <h4 class="fw-bold py-3 mb-4">
+      <span class="text-muted fw-light">Member /</span> Corporate
+    </h4>
+
+    <!-- DataTable with Buttons -->
+    <div class="row">
+        <div class="col-lg-12 mb-4 order-0">
+            <div class="card">
+                <div class="d-flex align-items-end row">
+                    <div class="col-sm-12">
+                        <div class="card-body">
+                            <table id="example" class="display" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Name</th>
+                                        <th>Membership ID</th>
+                                        <th>Email</th>
+                                        <th>Contact No</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($members as $member)
+                                    <tr>
+                                        <td>1</td>
+                                        <td>{{ $member->name }}</td>
+                                        <td>{{ $member->membership_number }}</td>
+                                        <td>{{ $member->email }}</td>
+                                        <td>{{ $member->mobile }}</td>
+                                        <td>
+                                            <a href="" class="btn btn-primary btn-sm"><i class="fa-solid fa-eye"></i></a>
+                                            <a href="" class="btn btn-success btn-sm"><i class="fa-solid fa-pen"></i></a>
+                                            <a href="" class="btn btn-danger btn-sm"><i class="fa-solid fa-trash"></i></a>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <!-- Total Revenue -->
+    </div>
+</div>
+
+@endsection
+
+@section('scripts')
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+
+<script>
+    $(document).ready(function () {
+        $('#example').DataTable();
+    });
+</script>
+@endsection
---
+++ resources/views/admin/members/show.blade.php
@@ -0,0 +1,180 @@
+@extends('layouts.backend')
+
+@section('content')
+    <div class="card mb-4">
+        <div class="card-body">
+            <div class="user-avatar-section">
+                <div class=" d-flex align-items-center flex-column">
+                    <img class="img-fluid rounded my-4" src="{{ asset('backend') }}/assets/img/avatars/10.png" height="110"
+                        width="110" alt="User avatar">
+                    <div class="user-info text-center">
+                        <h2 class="mb-2">{{ $member->name }}</h2>
+                        <span class="badge bg-label-secondary mb-2">{{ $member->member_type }}</span>
+                        <h6 class="">{{ $member->email }}</h6>
+                        <h6 class="mt-2">{{ $member->mobile }}</h6>
+                    </div>
+                </div>
+            </div>
+            {{-- <h5 class="pb-2 border-bottom mb-4">Member Details</h5> --}}
+            <div class="info-container mt-5">
+                <div class="row">
+                    <div class="col-6">
+                        <h5 class="pb-2 border-bottom mb-4">Member Details</h5>
+                        <ul class="list-unstyled">
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Membership Id:</span>
+                                <span>{{ $member->current_membership_number }}</span>
+                            </li>
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">National ID:</span>
+                                <span>{{ $member->nid_no }}</span>
+                            </li>
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Father Name:</span>
+                                <span>{{ $member->father_name }}</span>
+                            </li>
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Mother Name:</span>
+                                <span>{{ $member->mother_name }}</span>
+                            </li>
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Date of Birth:</span>
+                                <span>{{ $member->date_of_birth }}</span>
+                            </li>
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Nationality:</span>
+                                <span>{{ $member->nationality }}</span>
+                            </li>
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Marital Status:</span>
+                                <span>{{ $member->marital_status }}</span>
+                            </li>
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Spouse Name:</span>
+                                <span>{{ $member->spouse_ame }}</span>
+                            </li>
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Date of Marriage:</span>
+                                <span>{{ $member->date_of_marriage }}</span>
+                            </li>
+
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Road No:</span>
+                                <span>{{ $member->road_no }}</span>
+                            </li>
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Address:</span>
+                                <span>{{ $member->address }}</span>
+                            </li>
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Occupation:</span>
+                                <span>{{ $member->occupation }}</span>
+                            </li>
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Work Place:</span>
+                                <span>{{ $member->workplace }}</span>
+                            </li>
+
+
+                            <h5 class="pb-2 border-bottom mb-4 mt-5">Contact Person Details</h5>
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Name:</span>
+                                <span>{{ $member->contact_person_name }}</span>
+                            </li>
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Relation:</span>
+                                <span>{{ $member->relation }}</span>
+                            </li>
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Telephone:</span>
+                                <span>{{ $member->contact_person_telephone }}</span>
+                            </li>
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Mobile:</span>
+                                <span>{{ $member->contact_person_mobile }}</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-6">
+                        @foreach ($child_info as $child)
+                        <h5 class="pb-2 border-bottom mb-4">Children Details</h5>
+                        <ul class="list-unstyled mb-5">
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Name:</span>
+                                <span>{{ $child->children_name }}</span>
+                            </li>
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Age:</span>
+                                <span>{{ $child->age }}</span>
+                            </li>
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Gender:</span>
+                                <span>
+                                    @if($child->gender == 1)
+                                        {{ "Male" }}
+                                    @elseif($child->gender == 2)
+                                        {{ "Female" }}
+                                    @else
+                                        {{ "Others" }}
+                                    @endif
+                                </span>
+                            </li>
+                        </ul>
+                        @endforeach
+
+                        <h5 class="pb-2 border-bottom mb-4">Payment Details</h5>
+                        <ul class="list-unstyled">
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Payment Type:</span>
+                                <span>{{ $payment->payment_type }}</span>
+                            </li>
+
+                            {{--
+                            <li class="mb-3">
+                                <span class="fw-bold me-2">Payment Amount:</span>
+                                <span>
+                                    @if ($payment->category_of_membership == 1)
+                                        {{ 5000 }}
+                                    @elseif($payment->category_of_membership == 2)
+                                        {{ 50000 }}
+                                    @elseif($payment->category_of_membership == 3)
+                                        {{ 50000 }}
+                                    @elseif($payment->category_of_membership == 4)
+                                        {{ 0 }}
+                                    @elseif($payment->category_of_membership == 5)
+                                        {{ 250000 }}
+                                    @endif
+                                </span>
+                            </li> --}}
+
+                            @if ($payment->payment_type == 'cash')
+                                <li class="mb-3">
+                                    <span class="fw-bold me-2">Payment Date:</span>
+                                    <span>{{ $payment->payment_date }}</span>
+                                </li>
+                            @else
+                                <li class="mb-3">
+                                    <span class="fw-bold me-2">Bank Name:</span>
+                                    <span>{{ $payment->bank_name }}</span>
+                                </li>
+                                <li class="mb-3">
+                                    <span class="fw-bold me-2">Cheque Number:</span>
+                                    <span>{{ $payment->cheque_number }}</span>
+                                </li>
+                                <li class="mb-3">
+                                    <span class="fw-bold me-2">Payment Date:</span>
+                                    <span>{{ $payment->cheque_date }}</span>
+                                </li>
+                            @endif
+
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center pt-3">
+                    <a href="{{ route('member.executive') }}" class="btn btn-primary me-3">Back</a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
